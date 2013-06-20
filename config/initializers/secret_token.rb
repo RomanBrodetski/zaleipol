a9d2c597763fd8b7a14878258b8095adb552376a
unknown <roman.brodetski@gmail.com>
2013-06-21 01:14:09 +0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zaleipol::Application.config.secret_token = '5730e51893a896cfcd8cf1d332f233e20e65c744f437388a26cffec3483bd7ab580de1c614a475baa689df800996d2220b5bbc4bfa5062903f5e6bd1348e4a43'
